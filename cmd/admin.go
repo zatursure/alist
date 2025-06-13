3e781d1cf9327b719629ca80c2bfdaa263b3f0b6
Zatursure <im_zatursure@163.com>
2025-06-13 13:46:38 +0800

    Update admin.go

--- cmd/admin.go
+++ cmd/admin.go
@@ -1,14 +1,11 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"github.com/alist-org/alist/v3/internal/conf"
-	"github.com/alist-org/alist/v3/internal/op"
-	"github.com/alist-org/alist/v3/internal/setting"
-	"github.com/alist-org/alist/v3/pkg/utils"
-	"github.com/alist-org/alist/v3/pkg/utils/random"
+	"github.com/zatursure/mixlist/internal/conf"
+	"github.com/zatursure/mixlist/internal/op"
+	"github.com/zatursure/mixlist/internal/setting"
+	"github.com/zatursure/mixlist/pkg/utils"
+	"github.com/zatursure/mixlist/pkg/utils/random"
 	"github.com/spf13/cobra"
 )
 
